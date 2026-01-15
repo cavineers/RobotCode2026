b3625e7c332fbce282c308411a57e5fff3122319
Carmen <149629166+carmen-contreras@users.noreply.github.com>
2026-01-15 14:39:28 -0500

    prelim adds
    not running nor done jst made classes

---
+++ src/main/java/frc/robot/subsystems/Turret/TurretConstants.java
@@ -0,0 +1,12 @@
+public class TurretConstants {
+
+    //all currently placeholders, just getting setup for now!
+    public static final int turretLeftCanID = 1; 
+    public static final int turretRightCanID = 2;
+    public static final boolean kInverted = false;
+    
+    public static final double kMaxAngle = 180;
+    public static final double kMinAngle = 0;
+
+    
+}
