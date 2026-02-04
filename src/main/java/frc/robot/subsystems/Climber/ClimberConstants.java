f4fdaa481a29d70274270428e9501afad498e68f
mattheW0324 <156961259+mattheW0324@users.noreply.github.com>
2026-02-04 16:40:57 -0500

    Fix: added todos

--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -3,21 +3,21 @@ package frc.robot.subsystems.Climber;
 public class ClimberConstants {
     public static final boolean kTuningMode = true;
 
-    public static final int kClimberCanID = 51;
+    public static final int kClimberCanID = 51; //TODO: Update
 
-    public static final double kRestAbsoluteRotations = 0.0;
-    public static final double kDeployedAbsoluteRotations = 5.0; //TODO: Update these 3
-    public static final double kEngagedAbsoluteRotations = 2.5;
-    public static final double kClimberGearRatio = 5.0;
+    public static final double kRestAbsoluteRotations = 0.0; //TODO: Update
+    public static final double kDeployedAbsoluteRotations = 5.0; //TODO: Update
+    public static final double kEngagedAbsoluteRotations = 2.5; //TODO: Update
+    public static final double kClimberGearRatio = 5.0; //TODO: Update
 
     public static final double kRestMotorRotations = kClimberGearRatio * kRestAbsoluteRotations;
     public static final double kDeployedMotorRotations = kClimberGearRatio * kDeployedAbsoluteRotations;
     public static final double kEngagedMotorRotations = kClimberGearRatio * kEngagedAbsoluteRotations;
 
-    public static final double kProportionalGainSpark = 1.0;
-    public static final double kIntegralTermSpark = 0.0;
-    public static final double kDerivativeTermSpark = 1.0;
-    public static final double kGravityTermSpark = -0.6;
+    public static final double kProportionalGainSpark = 1.0; //TODO: Update
+    public static final double kIntegralTermSpark = 0.0; //TODO: Update
+    public static final double kDerivativeTermSpark = 1.0; //TODO: Update
+    public static final double kGravityTermSpark = -0.6; //TODO: Update
 
     public static final double kProportionalTermSim = 0.1;
     public static final double kDerivativeTermSim = 0.0;
